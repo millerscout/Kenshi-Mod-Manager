9d87a33d9f4656758caf1d9817e4c602c706f2f3
Marcos Miller Dantas <mmillerdantas@outlook.com>
2020-08-15 10:44:16 -0300

    added logs on general crashs

--- KenshiModTool/App.xaml.cs
+++ KenshiModTool/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿//using Microsoft.AspNetCore.Hosting;
+using Core;
+using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -11,18 +14,28 @@ namespace KenshiModTool
     /// </summary>
     public partial class App : Application
     {
-        public App()
+        public App() : base()
         {
-
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
             //var builder = WebServer.Builder
             //    .CreateWebHostBuilder();
             //var task = Task.Run(() =>
             //{
             //    builder.Run();
             //});
+        }
+
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unhandled exception occurred: \n" + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            File.AppendAllText(Constants.Errorfile, $"{DateTime.Now} -  {e.Exception.Message}.{Environment.NewLine}");
+            File.AppendAllText(Constants.Errorfile, $"{e.Exception.StackTrace} {Environment.NewLine}");
 
         }
-    
+
+
     }
-  
+
 }
+
